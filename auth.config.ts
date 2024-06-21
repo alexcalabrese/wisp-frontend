6f0ec41a23b05686f8a225d32315e0f32af01bda
Alex Calabrese <58480609+alexcalabrese@users.noreply.github.com>
2024-06-21 18:28:04 +0000

    Fixed github login provider error

--- auth.config.ts
+++ auth.config.ts
@@ -55,4 +55,15 @@ export default {
     //   },
     // }),
   ],
+    cookies: {
+    pkceCodeVerifier: {
+      name: "next-auth.pkce.code_verifier",
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: true,
+      },
+    },
+  },
 } satisfies NextAuthConfig
